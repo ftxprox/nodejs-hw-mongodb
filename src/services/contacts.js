a114b1abd7c4a86f5e88c6ab2202a39f87d8849c
HryhorievOA <ftxprox@gmail.com>
2025-02-06 10:21:00 +0200

    ppc15

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,25 +1,13 @@
 import { ContactsCollection } from '../db/models/contacts.js';
 
 export const getAllContacts = async () => {
-  try {
-    console.log('Starting getAllContacts...');
     const contacts = await ContactsCollection.find();
-    console.log('Fetched contacts:', contacts);
     return contacts;
-  } catch (error) {
-    console.error('Error fetching contacts:', error);
-    throw error;
-  }
 };
 
 export const getContactById = async (contactId) => {
-  try {
     const contact = await ContactsCollection.findById(contactId);
     return contact;
-  } catch (error) {
-    console.error('Error fetching contacts:', error);
-    throw error;
-  }
 };
 
 export const createContact = async (payload) => {
