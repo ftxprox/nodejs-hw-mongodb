4a4031464d378ca3a9d1298816ba5ecd0d710055
ftxprox <ftxprox@gmail.com>
2025-02-11 22:31:02 +0200

    niche ne poniatno

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -45,7 +45,14 @@ export const updateContact = async (contactId, payload, options = {}) => {
         },
     );
 
-}
+    if (!rawResult || !rawResult.value) return null;
+
+    return {
+        contact: rawResult.value,
+        isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+    };
+};
+
 export const deleteContact = async (contactId) => {
   const contact = await ContactsCollection.findOneAndDelete({
     _id: contactId,
