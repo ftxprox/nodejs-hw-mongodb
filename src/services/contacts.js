ce118f47aade69e80c1953342aaafe2ed552c5b5
ftxprox <ftxprox@gmail.com>
2025-02-11 22:22:43 +0200

    mega super ppc

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -15,24 +15,36 @@ export const createContact = async (payload) => {
   return contact;
 };
 
+//export const updateContact = async (contactId, payload, options = {}) => {
+//  const rawResult = await ContactsCollection.findOneAndUpdate(
+//    { _id: contactId },
+//    payload,
+//    {
+//      new: true,
+//      includeResultMetadata: true,
+//      ...options,
+//    },
+//  );
+
+//  if (!rawResult || !rawResult.value) return null;
+
+//  return {
+//    student: rawResult.value,
+//    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+//  };
+//};
+
 export const updateContact = async (contactId, payload, options = {}) => {
-  const rawResult = await ContactsCollection.findOneAndUpdate(
-    { _id: contactId },
-    payload,
-    {
-      new: true,
-      includeResultMetadata: true,
-      ...options,
-    },
-  );
+    const rawResult = await ContactsCollection.findOneAndUpdate(
+        { _id: contactId },
+        payload,
+        {
+            new: true,
+            includeResultMetadata: true,
+            ...options,
+        },
+    );
 
-  if (!rawResult || !rawResult.value) return null;
-
-  return {
-    student: rawResult.value,
-    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
-  };
-};
 
 export const deleteContact = async (contactId) => {
   const contact = await ContactsCollection.findOneAndDelete({
