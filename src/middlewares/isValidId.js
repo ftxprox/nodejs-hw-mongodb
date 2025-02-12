00dfc26cf03459a3542efe4d76cfd8c4f394dd9f
ftxprox <ftxprox@gmail.com>
2025-02-12 09:57:03 +0200

    plk

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -5,7 +5,7 @@ export const isValidId = (req, res, next) => {
   const { contactId } = req.params;
 
   if (!isValidObjectId(contactId)) {
-    return next(createHttpError(400, 'Invalid contact ID format));
+    return next(createHttpError(400, 'Invalid contact ID format'));
   }
 
   next();
