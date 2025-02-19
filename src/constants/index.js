3dda1cf173d5f6e4ee85c2871f11b84ca21f95ab
ftxprox <ftxprox@gmail.com>
2025-02-19 13:04:32 +0200

    swagger1

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,6 +3,7 @@ import path from 'node:path';
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const ONE_DAY = 24 * 60 * 60 * 1000;
 
+
 export const SMTP = {
     SMTP_HOST: 'SMTP_HOST',
     SMTP_PORT: 'SMTP_PORT',
@@ -12,10 +13,15 @@ export const SMTP = {
 };
 
 export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
+
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
 export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
+
 export const CLOUDINARY = {
     CLOUD_NAME: 'CLOUD_NAME',
     API_KEY: 'API_KEY',
     API_SECRET: 'API_SECRET',
 };
+
+
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
